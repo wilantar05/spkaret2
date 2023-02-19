fdae4ed7897838d4e8044103301094e6f1e23034
SayuGita <sayu@undiksha.ac.id>
2023-02-20 00:06:30 +0800

    [Krishna] UP DB

---
+++ spkaret.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2023 at 05:05 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spkaret`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_gejala`
+--
+
+CREATE TABLE `tb_gejala` (
+  `id_gejala` int(11) NOT NULL,
+  `nama_gejala` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penyakit`
+--
+
+CREATE TABLE `tb_penyakit` (
+  `id_penyakit` int(11) NOT NULL,
+  `nama_penyakit` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_rules`
+--
+
+CREATE TABLE `tb_rules` (
+  `id_rules` int(11) NOT NULL,
+  `id_gejala` int(11) NOT NULL,
+  `id_penyakit` int(11) NOT NULL,
+  `nilai_mb` int(11) NOT NULL,
+  `nilai_md` int(11) NOT NULL,
+  `nilai_cf` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tb_gejala`
+--
+ALTER TABLE `tb_gejala`
+  ADD PRIMARY KEY (`id_gejala`);
+
+--
+-- Indexes for table `tb_penyakit`
+--
+ALTER TABLE `tb_penyakit`
+  ADD PRIMARY KEY (`id_penyakit`);
+
+--
+-- Indexes for table `tb_rules`
+--
+ALTER TABLE `tb_rules`
+  ADD PRIMARY KEY (`id_rules`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_gejala`
+--
+ALTER TABLE `tb_gejala`
+  MODIFY `id_gejala` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_penyakit`
+--
+ALTER TABLE `tb_penyakit`
+  MODIFY `id_penyakit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_rules`
+--
+ALTER TABLE `tb_rules`
+  MODIFY `id_rules` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
